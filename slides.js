d28690f208efbbf1ca48a6eae904ae4362c289e2
Joe Versoza <jjv222@nyu.edu>
2018-02-08 10:57:09 -0500

    remove comments, use const

--- slides.js
+++ slides.js
@@ -13,10 +13,8 @@ class Slide {
     }
 
     hasNextFragment() {
-        // console.log('has next', this.fragments, this.fragmentIndex);
-        var hasNext = this.fragments.length > 0 && this.fragmentIndex !== undefined && this.fragmentIndex < this.fragments.length - 1;
-        var ret = hasNext || this.fragments.length > 0 && this.fragmentIndex === undefined;
-        // console.log('has next return', ret);
+        const hasNext = this.fragments.length > 0 && this.fragmentIndex !== undefined && this.fragmentIndex < this.fragments.length - 1;
+        const ret = hasNext || this.fragments.length > 0 && this.fragmentIndex === undefined;
         return ret;
     }
 
@@ -28,10 +26,8 @@ class Slide {
         if(this.fragments && this.fragmentIndex === undefined) {
             this.fragmentIndex = 0; 
             this.currentFragment = this.fragments[this.fragmentIndex];
-            // console.log(this.fragments, this.fragmentIndex);
             this.currentFragment.classList.add('active');
         } else {
-            // this.currentFragment.classList.remove('active');
             this.fragmentIndex += 1;
             this.currentFragment = this.fragments[this.fragmentIndex];
             this.currentFragment.classList.add('active');
@@ -55,7 +51,7 @@ class Slide {
 
 class SlideShow {
     constructor(container, tag) {
-        var slideTag = tag || 'section';
+        const slideTag = tag || 'section';
         this.slides = Array.prototype.map.call(container.querySelectorAll(slideTag), function(ele) {
             return new Slide(ele);
         });
@@ -63,24 +59,22 @@ class SlideShow {
 
     start() {
         if(this.slides) {
-            // console.log(window.location.hash.substr(1));
             window.addEventListener('keydown', this.handleKey.bind(this));
             window.addEventListener('hashchange', this.hashChange.bind(this));
-            var slideNum = parseInt(window.location.hash.substr(1));
-            var startSlide = isNaN(slideNum) ? 0 : slideNum;
+            const slideNum = parseInt(window.location.hash.substr(1));
+            const startSlide = isNaN(slideNum) ? 0 : slideNum;
             this.slide(startSlide); 
         } 
     }
 
     hashChange() {
-        var slideNum = parseInt(window.location.hash.substr(1));
+        const slideNum = parseInt(window.location.hash.substr(1));
         if(!isNaN(slideNum)) {
             this.slide(slideNum);
         }
     }
 
     handleKey(evt) {
-        // console.log(evt.key);
         if(evt.key === 'ArrowRight' || evt.keyCode === 39) {
             this.next();
         } else if (evt.key === 'ArrowLeft' || evt.keyCode === 37) {
@@ -91,16 +85,10 @@ class SlideShow {
     next() {
         if(this.activeIndex === undefined && this.activeSlide === undefined && this.slides.length > 0) {
             this.activeIndex = 0;
-            //this.activeSlide = this.slides[this.activeIndex];
-            //this.activeSlide.activate();
             window.location.hash = this.activeIndex;
         } else if(this.activeSlide.hasNextFragment()) { 
             this.activeSlide.nextFragment(); 
         } else if(this.activeIndex < this.slides.length - 1 && this.activeSlide) {
-            //this.activeSlide.deactivate();
-            //this.activeIndex += 1;
-            //this.activeSlide = this.slides[this.activeIndex];
-            //this.activeSlide.activate();
             window.location.hash = this.activeIndex + 1;
         }
     }
@@ -109,16 +97,11 @@ class SlideShow {
         if(this.activeSlide.hasPreviousFragment()) { 
             this.activeSlide.previousFragment();
         } else if(this.activeIndex > 0 && this.activeSlide) {
-            //this.activeSlide.deactivate();
-            //this.activeIndex -= 1;
-            //this.activeSlide = this.slides[this.activeIndex];
-            //this.activeSlide.activate();
             window.location.hash = this.activeIndex - 1;
         }
     }
 
     slide(i) {
-        // console.log('Going to slide index', i);
         if(i < this.slides.length && i >= 0) {
             if(this.activeSlide !== undefined) {
                 this.activeSlide.deactivate();
